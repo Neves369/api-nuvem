db5d78a4c507feb244d9b7c3fe64a523c68996ea
Neves369 <douglasbrian369@hotmail.com>
2024-07-26 15:21:51 -0300

    initial

---
+++ apiNuvem/src/main/java/com/dneves/apiNuvem/ApiNuvemApplication.java
@@ -0,0 +1,13 @@
+package com.dneves.apiNuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNuvemApplication.class, args);
+	}
+
+}
---
+++ apiNuvem/src/test/java/com/dneves/apiNuvem/ApiNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dneves.apiNuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
